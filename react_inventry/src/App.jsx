b22f2ba85a8192558415c78380af600158d2bdaf
Karishma282 <78778719+Karishma282@users.noreply.github.com>
2022-04-10 14:43:09 +0530

    Add files via upload

---
+++ react_inventry/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Inventory } from "../src/compoents/inventry";
+
+function App() {
+  return (
+    <div className="App">
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
