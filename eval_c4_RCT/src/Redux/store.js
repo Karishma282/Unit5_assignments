609137f3a465bf42537e2bcf02678850ef40992e
Karishma Nikam <78778719+Karishma282@users.noreply.github.com>
2022-06-13 16:39:23 +0530

    Update store.js

--- eval_c4_RCT/src/Redux/store.js
+++ eval_c4_RCT/src/Redux/store.js
@@ -4,7 +4,7 @@ import { legacy_createStore,
     compose,
  } from "redux";
 import thunk from 'redux-thunk';
-import CompanyReducer from './Company/reducer'
+import CompanyReducer from './company/reducer'
 const rootReducer=combineReducers({company:CompanyReducer})
 const composeEnhancers=window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 export const store=legacy_createStore(
